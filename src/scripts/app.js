943c077bf8cb290a76786ded75ebd1656e55a77f
alyssa-beaufils <alyssabeaufils@gmail.com>
2025-05-15 16:59:46 +0200

    style page login

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,12 +3,3 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.to('.logo', {
-    x: 300, 
-    duration: 3,
-    scrollTrigger: {
-        trigger: '.logo',
-        toggleActions: "play none none reverse",
-        start: "top center",
-    }
-});
